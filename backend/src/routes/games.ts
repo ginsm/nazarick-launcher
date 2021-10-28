d7e35dc9ba0e334eb6e2c3703cdd609a73274022
ginsm <mattg.0609@gmail.com>
2021-10-28 04:32:46 -0700

    respond with an empty object if gamelist.json empty/missing

--- backend/src/routes/games.ts
+++ backend/src/routes/games.ts
@@ -4,10 +4,5 @@ import { parseFile, hasProperties } from 'src/utilities';
 export default async function getGamelist(req, res) {
   const gamesDirectory = process.env.GAMES_ROOT;
   const games = await parseFile(path.join(gamesDirectory, 'gamelist.json'));
-
-  if (hasProperties(games)) {
-    return res.json(games);
-  }
-
-  res.status(500).json({ error: "Missing or empty: gamelist.json; has it been generated?" });
+  return res.json(games);
 }
