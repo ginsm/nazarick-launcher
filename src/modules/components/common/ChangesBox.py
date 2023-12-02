6f350f39ace983ca770cdefaf03d60860e635d35
Ginsm <mattg.0609@gmail.com>
2023-12-02 09:41:28 -0500

    Switch to tkinterweb from tkhtmlview
    TkinterWeb seems like a more complete, robust solution.
    
    There's only one bug I'm not sure how to fix currently:
    The initial html_frame.yview() seems to be off -- it's causing the
    scrollbar to appear smaller than it really is.

--- src/modules/components/common/ChangesBox.py
+++ src/modules/components/common/ChangesBox.py
@@ -1,97 +1,117 @@
 import os
+import webbrowser
 from modules.tufup import BASE_DIR
 from customtkinter.windows.widgets.theme import ThemeManager
 from markdown2 import Markdown
-from tkhtmlview import HTMLLabel
+from tkinterweb import HtmlFrame
 
 
 def create(ctk, parent, game):
-    background_color = ThemeManager.theme.get('CTkTextbox').get('fg_color')
-    text_color = ThemeManager.theme.get('CTkTextbox').get('text_color')
+    # Colors
+    colors = {
+        'background_color': ThemeManager.theme.get('CTkTextbox').get('fg_color'),
+        'text_color': ThemeManager.theme.get('CTkTextbox').get('text_color'),
+        'faint_text_color': ThemeManager.theme.get('CTkFrame').get('fg_color')
+    }
+
+    # Create the frame
+    changes = ctk.CTkFrame(
+        master=parent,
+        corner_radius=6,
+        border_width=0,
+        fg_color=colors.get('background_color'),
+    )
+
+    # Configure rows/columns
+    changes.grid_columnconfigure(0, weight=1)
+    changes.grid_rowconfigure(0, weight=1)
 
     # Check if CHANGELOG.md exists
     changelog_path = os.path.join(BASE_DIR, 'assets', game, 'CHANGELOG.md')
 
     if os.path.exists(changelog_path):
-        changes = ctk.CTkScrollableFrame(
-            master=parent,
-            corner_radius=6,
-            fg_color=background_color,
-        )
-
-        # Configure rows/columns
-        changes.grid_columnconfigure(0, weight=1)
-        changes.grid_rowconfigure(0, weight=1)
-
         with open(changelog_path, 'rb') as f:
             contents = f.read().decode('UTF-8')
             md2html = Markdown()
 
             # Get color mode
             mode = 1 if ctk.get_appearance_mode() == 'Dark' else 0
-            contents = style_tags(
-                html=md2html.convert(contents), 
-                tags=['h1', 'h2', 'h3', 'h4', 'h5', 'li', 'p', 'div'],
-                mode=mode
-            )
 
-            html = HTMLLabel(
-                changes,
-                background=background_color[mode],
-                html=contents
-            )
-            html.config(spacing3=6)
-            html.grid(row=0, column=0, padx=12, pady=(10, 6), sticky='nsew')
-            html.fit_height()
+            html_frame = HtmlFrame(master=changes, messages_enabled=False, vertical_scrollbar=False)
+            html_frame.load_html(get_stylesheet(colors, mode) + md2html.convert(contents))
+            html_frame.on_link_click(webbrowser.open)
+            html_frame.grid(row=0, column=0, padx=12, pady=(10, 6), sticky='nsew')
+            html_frame.yview_scroll(1, 'units')
+
+            html_scrollbar = ctk.CTkScrollbar(master=changes, command=html_frame.yview)
+            html_scrollbar.grid(row=0, column=1, pady=10, padx=5, sticky='ns')
+
+            html_frame.bind_all("<MouseWheel>", lambda e: update_scrollbar(e, html_scrollbar, html_frame))
+            # FIXME - Incorrect value being passed to scrollbar
+            html_frame.on_done_loading(lambda: html_scrollbar.set(*html_frame.yview()))
     
     else:
-        changes = ctk.CTkFrame(
-            master=parent,
-            corner_radius=6,
-            fg_color=background_color,
-        )
-
-        # Configure rows/columns
-        changes.grid_columnconfigure(0, weight=1)
-        changes.grid_rowconfigure(0, weight=1)
-
         label = ctk.CTkLabel(
             master=changes,
             text='Changes cannot be shown for this game.',
-            text_color=text_color,
+            text_color=colors.get('text_color'),
         )
         label.grid(row=0, column=0, padx=12, pady=6, sticky='nsew')
 
     return changes
 
-def style_tags(html, tags, mode):
-    font_sizes = {
-        'h1': 18,
-        'h2': 12,
-        'h3': 11,
-        'h4': 10,
-        'h5': 10,
-        'li': 10,
-        'p': 10
-    }
 
-    prominent = ThemeManager.theme.get('CTkTextbox').get('text_color')
-    faint = ThemeManager.theme.get('CTkFrame').get('fg_color')
+def update_scrollbar(event, scrollbar, frame):
+    scrollbar.set(*frame.yview())
+    frame.scroll(event)
 
-    font_colors = {
-        'h1': prominent,
-        'h2': prominent,
-        'h3': prominent,
-        'h4': prominent,
-        'h5': prominent,
-        'li': prominent,
-        'p': prominent,
-        'div': faint,
-    }
 
-    for tag in tags:
-        if tag in font_sizes:
-            html = html.replace(f'<{tag}', f'<{tag} style="color: {font_colors[tag][mode]}; font-size: {font_sizes[tag]}px;"')
-        else:
-            html = html.replace(f'<{tag}', f'<{tag} style="color: {font_colors[tag][mode]}; "')
-    return html
+def get_stylesheet(colors, mode):
+    background_color, faint_text_color, text_color = [
+        colors.get('background_color'),
+        colors.get('faint_text_color'),
+        colors.get('text_color')
+    ]
+
+    return f"""
+        <style>
+        html, body {{
+            padding: 0 0 10px 0;
+            background: {background_color[mode]};
+            scrollbar-color: red orange;
+            scrollbar-width: thin;
+        }}
+
+        hr {{
+            border: 1px solid {faint_text_color[mode]};
+            margin: 40px 0;
+        }}
+
+        h1 {{
+            font-size: 24px;
+            margin: 0;
+        }}
+
+        h2 {{
+            font-size: 16px;
+        }}
+
+        h2, h3 {{
+            margin: 24px 0 16px 0;
+        }}
+
+        ul, ol {{
+            margin: 0 0 10px 30px;
+            padding: 0;
+        }}
+
+        li {{
+            font-size: 13px;
+            margin-top: 8px;
+        }}
+
+        h1, h2, h3, li, p, div, a {{
+            color: {text_color[mode]};
+        }}
+    </style>
+    """
