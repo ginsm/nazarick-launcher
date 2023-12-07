73ca89f5f9a8df91228a6099bdaf5832b9883438
Ginsm <mattg.0609@gmail.com>
2023-12-06 19:56:31 -0500

    Fix scrollbar not updating issue
    The scrollbar wasn't updating when the user tried to scroll through the
    frame whilst hovering over the scrollbar itself. This commit fixes said
    issue.

--- src/modules/components/common/ChangesBox.py
+++ src/modules/components/common/ChangesBox.py
@@ -55,26 +55,33 @@ def load_changelog(ctk, changes, game, html_frame):
             html_frame.load_html(stylesheet + md2html.convert(contents))
 
             # Create scrollbar and bind necessary events
-            html_scrollbar = ctk.CTkScrollbar(master=changes, command=html_frame.yview)
-            html_scrollbar.grid(row=0, column=1, pady=10, padx=5, sticky='ns')
-            html_scrollbar.set(*html_frame.yview()) # Set initial value
+            scrollbar = ctk.CTkScrollbar(
+                master=changes,
+                command=lambda _, dist, unit: scroll_scrollbar(html_frame, scrollbar, dist, unit)
+            )
+            scrollbar.grid(row=0, column=1, pady=10, padx=5, sticky='ns')
+            scrollbar.set(*html_frame.yview()) # Set initial value
 
             # Event handling (scrolling)
-            html_frame.bind('<Enter>', lambda _: bind_to_mousewheel(html_frame, html_scrollbar))
+            html_frame.bind('<Enter>', lambda _: bind_to_mousewheel(html_frame, scrollbar, html_frame))
             html_frame.bind('<Leave>', lambda _: unbind_to_mousewheel(html_frame))
     else:
         html_frame.load_html(stylesheet + '<div>There is no change log available for this game.</div>')
 
 
-def bind_to_mousewheel(frame, scrollbar):
-    frame.bind_all("<MouseWheel>", lambda e: update_scrollbar(e, frame, scrollbar))
+def bind_to_mousewheel(frame, scrollbar, widget):
+    widget.bind_all("<MouseWheel>", lambda e: scroll_event(e, frame, scrollbar))
 
-def unbind_to_mousewheel(frame):
-    frame.unbind_all("<MouseWheel>")
+def unbind_to_mousewheel(widget):
+    widget.unbind_all("<MouseWheel>")
 
-def update_scrollbar(event, frame, scrollbar):
+def scroll_event(event, frame, scrollbar):
     scrollbar.set(*frame.yview())
     frame.scroll(event)
+
+def scroll_scrollbar(frame, scrollbar, dist, unit):
+    frame.yview_scroll(dist, unit)
+    scrollbar.set(*frame.yview())
 
 
 def get_stylesheet(colors, mode):
