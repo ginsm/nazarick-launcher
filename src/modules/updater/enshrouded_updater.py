167bf3d43a095484537c73246eacd6b5677cf952
Ginsm <mattg.0609@gmail.com>
2024-02-09 22:24:37 -0500

    Add functionality to the Enshrouded updater
    As there is nothing that needs to be done to the game, the launcher
    simply launches Enshrouded when the 'Play' button is pressed.

--- src/modules/updater/enshrouded_updater.py
+++ src/modules/updater/enshrouded_updater.py
@@ -1,2 +1,18 @@
+from modules import store, utility
+from modules.updater.common import run_executable
+
+# This game currently has no mods; thus the 'update' function is going to be pretty
+# barren for now.
 def start(ctk, app, pool, widgets):
+    log = widgets.get('logbox').get('log')
+    options = store.get_state()
+
+    log(f'')
+    log(f'[INFO] Beginning process at {utility.get_time()}.')
+
+
+    log(f'[INFO] This game doesn\'t require updates as it\'s vanilla only currently.')
+
+    run_executable(exe_name='enshrouded.exe', debug=options['debug'], log=log, command=['cmd', '/c', 'start', 'steam://run/1203620'])
+
     return;
