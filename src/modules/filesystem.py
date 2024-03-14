55654ceb18996d1fabe86abaf0b9558a334813ac
Ginsm <mattg.0609@gmail.com>
2024-03-14 17:04:38 -0400

    Move (can_)delete_path to filesystem from common
    This is definitely a filesystem call. No reason to be in common.py.

--- src/modules/filesystem.py
+++ src/modules/filesystem.py
@@ -37,6 +37,27 @@ def move_files(source, target, overwrite=True):
                 shutil.move(file_path, target_path)
 
 
+def delete_path(base_path, path, whitelist, log):
+    if can_delete_path(base_path, path, whitelist):
+        rm_func = shutil.rmtree if os.path.isdir(path) else os.remove
+        rm_func(path)
+        log(f'[INFO] (R) {path.replace(base_path, "")[1:]}')
+
+
+def overwrite(source, target):
+    target_root, _ = os.path.split(target)
+    rm_func = shutil.rmtree if os.path.isdir(target) else os.remove
+
+    if os.path.exists(target):
+        rm_func(target)
+
+    if not os.path.exists(target_root):
+        os.makedirs(target_root)
+
+    shutil.move(source, target)
+
+
+# ---- HELPER FUNCTIONS ---- #
 def path_is_relative(base, path):
     # Get the absolute paths
     base_path = os.path.abspath(base)
@@ -47,3 +68,33 @@ def path_is_relative(base, path):
     if abs_path[0:base_len] != base_path:
         return False
     return True
+
+
+def can_delete_path(base_path, path, whitelist = []):
+    """
+        Ensures that the given path is within a whitelisted directory inside the instance path and that the path exists.\n\n
+    """
+    # Variables
+    path_abs = os.path.abspath(path)
+    result = False
+                
+    # Determine if the path is within the base path
+    if not path_is_relative(base_path, path_abs):
+        return False
+
+    # Ensure the path exists
+    if not os.path.exists(path_abs):
+        return False
+
+    # Ensure the file to delete is within a whitelisted directory
+    # while ensuring the target isn't the directory itself.
+    for dir_ in whitelist:
+        base_whitelist_path = os.path.join(base_path, dir_)
+        if path_is_relative(base_whitelist_path, path_abs) and base_whitelist_path != path_abs:
+            result = True
+
+    # 
+    if len(whitelist) == 0:
+        result = True
+
+    return result
