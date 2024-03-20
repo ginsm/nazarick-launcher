94fdf5ea46b653fe79a0e24f249b1ffc73a778f1
Ginsm <mattg.0609@gmail.com>
2024-03-20 04:07:12 -0400

    Fix typo: logging.logRecord -> logging.LogRecord

--- src/modules/logging/filters/NoBroadcastFilter.py
+++ src/modules/logging/filters/NoBroadcastFilter.py
@@ -4,5 +4,5 @@ from typing import override
 
 class Filter(logging.Filter):
     @override
-    def filter(self, record: logging.logRecord) -> bool | logging.LogRecord:
+    def filter(self, record: logging.LogRecord) -> bool | logging.LogRecord:
         return not hasattr(record, 'broadcast')
