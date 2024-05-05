81af2ed128e84fed8a38fc55047a3e61d6ea288f
Ginsm <mattg.0609@gmail.com>
2024-05-05 05:56:09 -0400

    Working Fallout76Updater
    There is an issue with `install_update`; if the client already has some
    of the mods it will fail due to `shutil.copytree` saying permission
    denied. It seems to have a particular issue with ReShade's `.undef`
    files.
    
    I may just add an `initial_install` that checks if ReShade exists
    already and delete it. But it may be better to just create a custom copy
    function for `shutil.copytree` and handle exceptions on a per-file
    basis.

--- src/modules/updaters/Fallout76Updater.py
+++ src/modules/updaters/Fallout76Updater.py
@@ -1,5 +1,7 @@
+import json
 import os
-from modules import state_manager
+import shutil
+from modules import filesystem, state_manager
 from modules.updaters.AbstractGameUpdater import AbstractGameUpdater
 
 
@@ -10,11 +12,12 @@ class Fallout76Updater(AbstractGameUpdater):
 
         # Begin initializing variables
         self.game = 'Fallout76'
+        self.downloads_mods = False
 
         # Paths used throughout update
         self.temp_path = os.path.join(self.root, '_update_tmp', 'fallout76', self.modpack.get('name'))
         self.install_path = game_state.get('install')
-        self.documents_path = game_state.get('documents_path')
+        self.documents_path = game_state.get('documents')
         self.nazarick_json_path = os.path.join(self.install_path, 'nazarick.json')
 
         # The platform can either be 'Steam' or 'Microsoft Store'
@@ -48,5 +51,40 @@ class Fallout76Updater(AbstractGameUpdater):
             self.command = ['cmd', '/c', 'start', os.path.join(self.install_path, self.exe_name)]
 
 
+    def pre_update(self):
+        if os.path.exists(self.nazarick_json_path):
+            with open(self.nazarick_json_path, 'r') as file:
+                # Get mod index
+                contents = json.loads(file.read())
+                mods = contents.get('mod_index')
+
+                # Remove previously installed mods
+                if mods:
+                    self.logger.info('Removing previously installed mods.')
+                    for mod in mods:
+                        mod_path = os.path.join(self.install_path, mod)
+
+                        # Check if mod exists before deleting
+                        if os.path.exists(mod_path):
+                            filesystem.safe_delete(
+                                path=mod_path,
+                                base_path=self.install_path, 
+                                whitelist=[],
+                                logger=self.logger
+                            )
+                            self.logger.debug(f'(R) {mod}')
+
+
+    def resolve_mod_index(self, mods, _):
+        return mods
+
+
     def install_update(self):
-        pass
+        # Move modifications to install path (merging via shutil)
+        modifications_path = os.path.join(self.temp_mods_path, 'Modifications')
+        shutil.copytree(modifications_path, self.install_path, dirs_exist_ok=True)
+
+        # Move Fallout76Custom.ini into Fallout 76 documents
+        doc_path = os.path.join(self.temp_path, 'Fallout76Custom.ini')
+        doc_dest_path = os.path.join(self.documents_path, 'My Games', 'Fallout 76', 'Fallout76Custom.ini')
+        filesystem.overwrite_path(doc_path, doc_dest_path)
