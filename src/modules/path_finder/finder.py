cd0677cfd39715fe2546a30e3e7ee6d60bd45e69
Ginsm <mattg.0609@gmail.com>
2024-05-11 08:34:28 -0400

    Clean up path finder code
    The code is much more modular now and works based on AppID conventions.
    This simplifies the code as you only need to use `find_appid_path` to
    find steam game paths, microsoft app paths, and other start menu paths.
    
    In addition, the steam game path finder is now more intelligent and does
    not require a "directory" to be passed to it. It will look for the
    game's `appmanifest` file to determine the directory name.

--- src/modules/path_finder/finder.py
+++ src/modules/path_finder/finder.py
@@ -5,7 +5,8 @@ import vdf
 import subprocess
 from modules import constants
 
-# ---- STEAM ---- #
+
+# ---- Steam Specific ---- #
 def get_steam_path():
     # Finds the path on Windows by checking registry
     if constants.ON_WINDOWS:
@@ -16,17 +17,28 @@ def get_steam_path():
         return steam_path
 
 
-def get_steam_game_path(game_id, directory):
+def get_steam_game_path(game_id):
     steam_path = get_steam_path()
     libraries_path = os.path.join(steam_path, 'steamapps', 'libraryfolders.vdf')
     libraries_data = vdf.parse(open(libraries_path)).get('libraryfolders')
+
+    # Handle steam AppIDs
+    if game_id.startswith('steam://run'):
+        game_id = game_id.split("/")[-1]
 
     # Search for steam library containing game id
     for library_id in libraries_data:
         library = libraries_data[library_id]
 
         if game_id in library.get('apps'):
-            game_path = os.path.join(library.get('path'), 'steamapps', 'common', directory)
+            manifest_path = os.path.join(library.get('path'), 'steamapps', f'appmanifest_{game_id}.acf')
+
+            # Parse manifest to find the game's install directory
+            with open(manifest_path, 'r') as fp:
+                install_dir = vdf.loads(fp.read()).get('AppState').get('installdir')
+
+            # Create path to install directory
+            game_path = os.path.join(library.get('path'), 'steamapps', 'common', install_dir)
 
             # Check if game path exists and return it
             if os.path.exists(game_path):
@@ -36,7 +48,7 @@ def get_steam_game_path(game_id, directory):
     return False
 
 
-# ---- PowerShell (Windows Only) ---- #
+# ---- Windows Specific ---- #
 def run_powershell_command(command):
     # This can only be used on windows
     if not constants.ON_WINDOWS:
@@ -58,62 +70,83 @@ def get_win_folder(folder_id):
     if constants.ON_WINDOWS:
         import modules.path_finder.windows_paths as win_paths
 
-        # Convert string folder_ids to proper format
-        if type(folder_id) is str:
-            # Convert GUID strings to UUID
-            if folder_id.startswith("{"):
-                folder_id = UUID(folder_id)
-            # Handle plain-text folders, i.e. Documents, MyDocuments, Programs, etc.
-            else:
-                # Search the environment via PowerShell. This provides more accurate results.
-                # Run "[enum]::GetNames( [System.Environment+SpecialFolder] )" in PowerShell to
-                # see available folder locations.
-                try: 
-                    return run_powershell_command(f'[Environment]::GetFolderPath("{folder_id}")')
-                # Fallback to searching the FOLDERID class for the folder id
-                except:
-                    folder_id = getattr(win_paths.FOLDERID, folder_id)
-
-        return win_paths.get_path(folder_id)
+        # Search the environment via PowerShell. This provides more accurate results.
+        # Run "[enum]::GetNames( [System.Environment+SpecialFolder] )" in PowerShell to
+        # see available folder locations.
+        try: 
+            return run_powershell_command(f'[Environment]::GetFolderPath("{folder_id}")')
+        # Fallback to searching the FOLDERID class for the folder id and using get_path
+        except:
+            folder_id = getattr(win_paths.FOLDERID, folder_id)
+            return win_paths.get_path(folder_id)
 
 
-def get_microsoft_app_dir(app_id):
+def get_appid_from_startmenu(name):
+    """ Gets the Start Menu AppID for an app (must be an exact match). """
+    if constants.ON_WINDOWS:
+        cmd = "(get-StartApps) | Where-Object { $_.Name -eq \"" + name + "\" } | Select-Object -ExpandProperty AppID"
+        return run_powershell_command(cmd)
+
+
+def handle_knownfile_appids(appid):
+    if constants.ON_WINDOWS:
+        import modules.path_finder.windows_paths as win_paths
+        str_id = appid[0:appid.find("}") + 1]
+        guid = UUID(str_id)
+        return appid.replace(str_id, win_paths.get_path(guid))
+
+
+def handle_storeapp_appids(appid):
+    appid = appid[0:appid.find('!')]
     if constants.ON_WINDOWS:
         # The app dir typically has a version number sandwiched by the ID, i.e. for Minecraft Launcher:
         # AppID: Microsoft.4297127D64EC6_8wekyb3d8bbwe
         # Folder: Microsoft.4297127D64EC6_1.7.2.0_x64__8wekyb3d8bbwe
-        split_path = app_id.split('_')
+        split_path = appid.split('_')
         windows_apps_path = os.path.join(get_win_folder("ProgramFiles"), 'WindowsApps')
-        result = False
+        game_dir = False
 
         for app_dir in os.listdir(windows_apps_path):
             # Read above; this checks for a folder containing the ID sandwiching a version number.
             if app_dir.startswith(split_path[0]) and app_dir.endswith(split_path[-1]):
-                result = os.path.join(windows_apps_path, app_dir)
+                game_dir = os.path.join(windows_apps_path, app_dir)
 
-        return result
+        return game_dir
 
 
-def get_startmenu_game_path(name, exe=''):
-    if constants.ON_WINDOWS:
-        # Get the AppID via PowerShell's get-StartApps
-        app_id = run_powershell_command("(get-StartApps) | Where-Object { $_.Name -eq \"" + name + "\" } | Select-Object -ExpandProperty AppID")
+# ---- Main Functionality ---- #
+def get_appid_convention(string):
+    """ Determines the AppID convention. 
+    
+    Conventions: `path`, `application`, `steam`, `knownfile`, and `storeapp`. """
+    conventions = {
+        # r'^[\w]+\.[\w.]+$': 'namespace',
+        r'^[A-Za-z]:\\.*\\.*\.exe$': 'path',
+        r'^[A-Za-z0-9]+$': 'application',
+        r'^steam:\/\/(run|rungameid)\/\d+$': 'steam',
+        r'\{.*\}': 'knownfile',
+        r'^.*![\w\.]+$': 'storeapp'
+    }
 
-        # Ensures the AppID ends with either .exe or !* pattern
-        if not app_id.endswith('.exe') and not re.search(r'!.*$', app_id):
-            raise Exception(f"Unhandled AppID for {name}: {app_id}.")
-        
-        # Handle KNOWNFOLDERID AppIDs
-        if app_id.startswith("{"):
-            import modules.path_finder.windows_paths as win_paths
-            str_id = app_id[0:app_id.find("}") + 1] # Get only the UUID
-            win_path = win_paths.get_path(UUID(str_id))
-            app_id = app_id.replace(str_id, win_path) # Replace the UUID with the actual windows path
-            
-        # Handle Microsoft Store Apps' AppIDs
-        if re.search(r'!.*$', app_id):
-            app_id = app_id[0:app_id.find("!")] # Remove !* from the end of the line
-            game_dir = get_microsoft_app_dir(app_id)
-            app_id = os.path.join(game_dir, exe) if game_dir else False
+    for pattern, name in conventions.items():
+        if re.match(pattern, string):
+            return name
+    return None
 
-        return app_id
+
+def find_appid_path(appid):
+    """ Searches for the path based on the AppID and its convention. """
+    convention = get_appid_convention(appid)
+    match convention:
+        case 'path':
+            return appid
+        case 'application':
+            return appid
+        case 'steam':
+            return get_steam_game_path(appid)
+        case 'knownfile':
+            return handle_knownfile_appids(appid)
+        case 'storeapp':
+            return handle_storeapp_appids(appid)
+        case _:
+            raise Exception(f'AppID type is unhandled: {appid}')
