44746a163c3ccb036cfea50f311ba72787dfe2bc
Ginsm <mattg.0609@gmail.com>
2024-05-16 16:12:00 -0400

    Allow custom text on cover frames

--- src/modules/components/common/CoverFrame.py
+++ src/modules/components/common/CoverFrame.py
@@ -1,9 +1,9 @@
-def create(ctk, app):
+def create(ctk, app, text='Loading...'):
     cover_frame = ctk.CTkFrame(master=app, corner_radius=0, border_width=0)
     cover_frame.grid_columnconfigure(0, weight=1)
     cover_frame.grid_rowconfigure(0, weight=1)
 
-    loading_label = ctk.CTkLabel(master=cover_frame, text='Loading...')
+    loading_label = ctk.CTkLabel(master=cover_frame, text=text)
     loading_label.grid(row=0, column=0, sticky='nsew')
 
     cover_frame.grid(row=0, column=0, rowspan=5, columnspan=5, sticky='nsew')
